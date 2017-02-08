3020a05fd1e2c7e38a5a62de151da7225d1ffe43
Luviana <lutfianadewi5@gmail.com>
2017-02-08 15:14:30 +0700

    Fragment Change View 3 Fragment

--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment2/MainActivity.java
@@ -6,13 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import static android.R.string.no;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View2Fragment.IListener
 {
     private  int mNo;
     private TextView tvNumber;
+
+    @Override
+    public void toPromoPage()
+    {
+        changePage(5);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -45,7 +50,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                if(mNo<3)
+                if(mNo<4)
                 {
                     changePage(++mNo);
                 }
@@ -69,6 +74,8 @@ public class MainActivity extends AppCompatActivity
             case 1 : fragment = View1Fragment.newInstance(R.drawable.ic_brightness_5_black_24dp, "Matahari"); break;
             case 2 : fragment = View1Fragment.newInstance(R.drawable.ic_brightness_2_black_24dp, "Bulan"); break;
             case 3 : fragment = View1Fragment.newInstance(R.drawable.ic_wb_cloudy_black_24dp, "Awan"); break;
+            case 4 : fragment = new View2Fragment(); break;
+            case 5 : fragment = new View3Fragment(); break;
         }
         fm.beginTransaction().replace(R.id.container,fragment).commitNow();
     }
